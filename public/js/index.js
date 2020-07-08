3d522cb530e93d760f5d781a8b2c29a7b5681926
Adam Podoxin <adam.podoxin@gmail.com>
2020-07-08 10:43:17 -0700

    Create basic homepage
    This displays user's avatar, name, and login.

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,22 @@ const socket = io();
 
 let userInput;
 
+const getUser = () => {
+  socket.emit("get user", userInput.value);
+};
+
+const loadUser = (user) => {
+  const avatarImg = document.getElementById("user-avatar");
+  const nameText = document.getElementById("user-name");
+  const loginText = document.getElementById("user-login");
+
+  avatarImg.src = user.avatar_url;
+  nameText.innerHTML = user.name;
+  loginText.innerHTML = user.login;
+};
+
 window.onload = () => {
   userInput = document.getElementById("get-user-input");
 };
 
-socket.on("return user", (data) => {
-  console.log(data);
-});
-
-const getUser = () => {
-  socket.emit("get user", userInput.value);
-};
+socket.on("return user", (user) => loadUser(user));
