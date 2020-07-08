f9594f858fc07d1f285c7ea22ba5ba66db5509f3
Adam Podoxin <adam.podoxin@gmail.com>
2020-07-07 23:30:35 -0700

    Update dependence from Github API to https request

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const GitHub = require("github-api");
-const gh = new GitHub();
+const https = require("https");
 
 router.get("/", (req, res) => {
-  gh.getUser(req.body.user)
-    .listRepos()
-    .then(({ data: reposJson }) => {
-      res.json(reposJson);
-    });
+  const options = {
+    hostname: "https://api.github.com",
+    path: `users/${req.body.user}`,
+    headers: {
+      "User-Agent": "AdamPodoxin",
+    },
+  };
+
+  https
+    .get(options, (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        console.log(data);
+      });
+    })
+    .on("error", (err) => console.error(err));
 });
 
 module.exports = router;
