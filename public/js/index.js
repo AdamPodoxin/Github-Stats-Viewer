d5605071c451134083bfd6bec73dcd7dd96c1984
Adam Podoxin <adam.podoxin@gmail.com>
2020-07-15 22:16:46 -0700

    Implement basic sFront integration

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,8 @@
-let userInput;
+import * as sFront from "https://adampodoxin.github.io/sFront/sFront.js";
+
+const repoTemplatePath = "../templates/repo.html";
+
+let userInput, reposDiv;
 
 const getUser = () => {
   getApiRequest(`users/${userInput.value}`, (user) => loadUser(user));
@@ -12,8 +16,25 @@ const loadUser = (user) => {
   avatarImg.src = user.avatar_url;
   nameText.innerHTML = user.name;
   loginText.innerHTML = user.login;
+
+  Array.from(user.repos).forEach((repo) => {
+    const variables = {
+      name: repo.name,
+      description: repo.description,
+      githubURL: repo.html_url,
+    };
+
+    sFront.createElementFromTemplate(repoTemplatePath, variables, reposDiv);
+  });
+};
+
+const loadGithubRepo = (repoURL) => {
+  window.open(repoURL, "_blank");
 };
 
 window.onload = () => {
   userInput = document.getElementById("get-user-input");
+  reposDiv = document.querySelector("#repos");
+
+  sFront.registerFunctionsInWindow({ getUser, loadGithubRepo });
 };
