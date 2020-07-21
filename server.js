30dbaa5e370d84999b284f72d3317a9ad4f22192
Adam Podoxin <adam.podoxin@gmail.com>
2020-07-21 01:17:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const http = require("http");
-
 const usersRoute = require("./routes/users");
 
 const app = express();
-app.listen(5000 || process.env.PORT);
+app.listen(process.env.PORT || 5000);
 
 app.use(bodyParser.json());
 app.use("/api/users/*", usersRoute);
