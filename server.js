f9e5e6f7a90af9584a03925d319d3bba6330aa03
Adam Podoxin <adam.podoxin@gmail.com>
2020-07-21 00:59:11 -0700

    Attempted fix for port

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const bodyParser = require("body-parser");
 
 const http = require("http");
 
-const PORT = 5000 || process.env.PORT;
-
 const usersRoute = require("./routes/users");
 
 const app = express();
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(5000 || process.env.PORT);
 
 app.use(bodyParser.json());
 app.use("/api/users/*", usersRoute);
